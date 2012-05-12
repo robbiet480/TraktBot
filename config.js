907507bc8618a650584da0673061d4c1196b9276
Robbie Trencheny <me@robbiet.us>
2012-05-12 00:27:00 -0600

    initial1

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+config.server = 'irc.broadcasthe.net';
+config.nick = 'TVBot';
+config.channel = '#robbie-test';
+config.apikey = 'e3c4a73b474cb79740c67e31a1d10450';
+
+
+module.exports = config;
